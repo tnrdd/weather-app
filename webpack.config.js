b17301018ef3f36cda2fb5a7de95679ff656142e
tnrdd <tnrdd@tutanota.com>
2022-03-23 20:40:10 +0000

    add dist to gitignore

--- webpack.config.js
+++ webpack.config.js
@@ -4,7 +4,6 @@ const HtmlWebpackPlugin = require("html-webpack-plugin");
 module.exports = {
 	mode: "development",
   entry: {
-		//index: ["./src/index.js", "./src/print.js"],
 		index: ["./src/index.js"],
 },
 	plugins: [
